f4182f58988cc8b2e1164b40e670fe4487801253
Klintend <forrestuffoh@gmail.com>
2022-04-09 16:45:44 -0700

    data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)  
+ *
+ */
+int main(void)
+{
+int i;  
+char c;
+float f;
+long int li;
+long long int lli;
+printf("size of an int: %d byte(s)\n", (int)sizeof(i));
+printf("size of a char: %lu byte(s)\n", ( unsigned long)sizeof(c));
+printf("size of a float: %g byte(s)\n", (float)sizeof(f));
+printf("size of a long int: %ld byte(s)\n", (long int)sizeof(li));
+printf("size of a long long int: %lld byte(s)\n", (long long int)sizeof(lli));
+return (0);
+}
