8efb8a74ae34e7e4130cce3232c6a9120cced418
Klintend <forrestuffoh@gmail.com>
2022-04-09 17:18:28 -0700

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ long long int lli;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of an int: %d byte(s)\n", (int)sizeof(i));
 printf("size of a long int: %ld byte(s)\n", (long int)sizeof(li));
-printf("size of a long long int: %ldd byte(s)\n", (long long int)sizeof(lli));
+printf("size of a long long int: %lld byte(s)\n", (long long int)sizeof(lli));
 printf("size of a float: %g byte(s)\n", (float)sizeof(f));
 return (0);
 }
