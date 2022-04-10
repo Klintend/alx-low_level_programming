dd47ab2d043cf89c56e5db2b6dd2ee7b4bd81773
Klintend <forrestuffoh@gmail.com>
2022-04-09 19:22:02 -0700

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,14 +10,18 @@ int main(void)
 {
 int i;
 char str[40] = "size of a char: ";
+char str1[40] = "size of an int: ";
+char str2[40] = "size of a long int: ";
+char str3[40] = "size of a long long int: ";
+char str4[40] = "size of a float: ";
 float f;
 long int li;
 long long int lli;
 char c;
-printf("%s", str, "%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of an int: %d byte(s)\n", (int)sizeof(i));
-printf("size of a long int: %ld byte(s)\n", (long int)sizeof(li));
-printf("size of a long long int: %lld byte(s)\n", (long long int)sizeof(lli));
-printf("size of a float: %g byte(s)\n", (float)sizeof(f));
+printf("%s""%lu byte(s)\n", str, (unsigned long)sizeof(c));
+printf("%s""%d byte(s)\n", str1, (int)sizeof(i));
+printf("%s""%ld byte(s)\n", str2, (long int)sizeof(li));
+printf("%s""%lld byte(s)\n", str3, (long long int)sizeof(lli));
+printf("%s""%g byte(s)\n", str4, (float)sizeof(f));
 return (0);
 }
