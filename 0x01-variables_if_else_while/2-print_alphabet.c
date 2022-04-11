094ba7090228c436667c96c6f51f0400e60d9ce3
Klintend <forrestuffoh@gmail.com>
2022-04-10 22:49:10 -0700

    using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Printing Alphabets in lowercase
  *
  * Return: Always 0 (Success)
- *
  */
-int main()
+int main(void)
 {
 
 char ch;
@@ -20,5 +18,5 @@ for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 }
 putchar(ASCII);
-return 0;
+return (0);
 }
