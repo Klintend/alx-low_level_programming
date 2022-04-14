274ecbfefa5d0302d6f4e0e3785cba09475b5a60
Klintend <forrestuffoh@gmail.com>
2022-04-14 06:49:32 -0700

    debugging2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
+ * printf - print infinite loop incoming
+ * printf - print infinite loop avoided! \\o/\n
  * Return: 0
  */
-
 int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n)");
 /**
+ * printf - print infinite loop avoided! \\o/\n
+ * @'\\o/\n"': print '\\o/\n"'
  * i = 0;
- *
- *
  * while (i < 10)
  * {
  * putchar(i);
  * }
  */
- 
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
