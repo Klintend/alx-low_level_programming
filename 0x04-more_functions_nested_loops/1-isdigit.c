25187d3b579478d398dfd569e5c3b7c2e28d760e
Klintend <forrestuffoh@gmail.com>
2022-04-16 11:02:04 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit -checks for a digit (0 through 9)
+ * @c: The digit to print
+ * Return: 1, otherwise 0
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
